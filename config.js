04916d4512a1371593217a941bb0a3473a9f1658
Sashleo <daudpotaareesha@gmail.com>
2026-05-12 21:37:46 +0500

    Create config.js
    lets see if this fixes it

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js - Your own Supabase keys
+window.FALAAH_CONFIG = {
+    SUPABASE_URL: 'https://hqhwphrckizfnkthdzfx.supabase.co',
+    ANON_KEY: 'sb_publishable_A_uHsj9_CSvnNbMzaA74vA_qey-gTRu'
+};
