f249bef15acaad68298f1f408ea846fdedb1c135
Sashleo <daudpotaareesha@gmail.com>
2026-05-25 23:57:15 +0500

    Delete config.js
    made the repo public

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js - Your own Supabase keys
-window.FALAAH_CONFIG = {
-    SUPABASE_URL: 'https://hqhwphrckizfnkthdzfx.supabase.co',
-    ANON_KEY: 'sb_publishable_A_uHsj9_CSvnNbMzaA74vA_qey-gTRu'
-};
